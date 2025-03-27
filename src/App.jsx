3b8db5d367a267806610b5ba92a8d6177e6170f0
Divyansh chaudhary <hypr123890@gmail.com>
2025-03-27 23:04:24 +0530

    add ref to the inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
+
   const OTP_Length = 4;
   const [count, setCount] = useState(new Array(OTP_Length).fill(""));
+  const refArr = useRef(new Array(OTP_Length).fill(null));
 
-  console.log(count);
+
+  useEffect(() => {
+    refArr.current[0]?.focus();
+  },[])
+
 
   function handleOnChange(value,idx){
+    if(isNaN(value)) return;
     let newCount = [...count];
     newCount[idx] = value;
     setCount(newCount)
+
+    if(value && idx < OTP_Length-1){
+      refArr.current[idx+1].focus();
+    }
   }
 
   return (
@@ -18,12 +29,15 @@ function App() {
       <h1>OTP Validation</h1>
       {
         count.map((input,idx) => (
-          <input 
+          <input
             key={idx}
+            ref={(el)=> (refArr.current[idx] = el)}
             className='input-group'
-            type="text" 
+            type="text"
             value={count[idx]}
             onChange={(e)=> handleOnChange(e.target.value,idx)}
+            onKeyDown={(e) => handleOnKeyDown(e,idx)}
+            maxLength={1}
           />
         ))
       }
